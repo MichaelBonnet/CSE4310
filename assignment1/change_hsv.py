6deac2a49f984a5d0f580ff162a9ac15f181a79c
Michael Bonnet <maabonnet@gmail.com>
2022-02-02 14:05:27 -0600

    half of img_transforms is working, other half untested or not working

--- assignment1/change_hsv.py
+++ assignment1/change_hsv.py
@@ -1,62 +1,9 @@
 import sys
+from tkinter import X
 import numpy as np
 import PIL
 from PIL import Image
 from matplotlib import pyplot as plt
-
-# from Nikolay Polyarni
-
-# def rgb_to_hsv(rgb):
-
-#     input_shape = rgb.shape
-#     rgb = rgb.reshape(-1, 3)
-#     r, g, b = rgb[:, 0], rgb[:, 1], rgb[:, 2]
-
-#     maxc = np.maximum(np.maximum(r, g), b)
-#     minc = np.minimum(np.minimum(r, g), b)
-#     v = maxc
-
-#     deltac = maxc - minc
-#     s = deltac / maxc
-#     deltac[deltac == 0] = 1  # to not divide by zero (those results in any way would be overridden in next lines)
-#     rc = (maxc - r) / deltac
-#     gc = (maxc - g) / deltac
-#     bc = (maxc - b) / deltac
-
-#     h = 4.0 + gc - rc
-#     h[g == maxc] = 2.0 + rc[g == maxc] - bc[g == maxc]
-#     h[r == maxc] = bc[r == maxc] - gc[r == maxc]
-#     h[minc == maxc] = 0.0
-
-#     h = (h / 6.0) % 1.0
-#     res = np.dstack([h, s, v])
-#     return res.reshape(input_shape)
-
-
-# def hsv_to_rgb(hsv):
-
-#     input_shape = hsv.shape
-#     hsv = hsv.reshape(-1, 3)
-#     h, s, v = hsv[:, 0], hsv[:, 1], hsv[:, 2]
-
-#     i = np.int32(h * 6.0)
-#     f = (h * 6.0) - i
-#     p = v * (1.0 - s)
-#     q = v * (1.0 - s * f)
-#     t = v * (1.0 - s * (1.0 - f))
-#     i = i % 6
-
-#     rgb = np.zeros_like(hsv)
-#     v, t, p, q = v.reshape(-1, 1), t.reshape(-1, 1), p.reshape(-1, 1), q.reshape(-1, 1)
-#     rgb[i == 0] = np.hstack([v, t, p])[i == 0]
-#     rgb[i == 1] = np.hstack([q, v, p])[i == 1]
-#     rgb[i == 2] = np.hstack([p, v, t])[i == 2]
-#     rgb[i == 3] = np.hstack([p, q, v])[i == 3]
-#     rgb[i == 4] = np.hstack([t, p, v])[i == 4]
-#     rgb[i == 5] = np.hstack([v, p, q])[i == 5]
-#     rgb[s == 0.0] = np.hstack([v, v, v])[s == 0.0]
-
-#     return rgb.reshape(input_shape)
 
 # Takes an RGB image represented as a numpy array (rgb_image)
 # And converts it into HSV format.
@@ -68,16 +15,16 @@ def rgb_to_hsv_1( rgb_image ):
     g = rgb_image[:,:,1]
     b = rgb_image[:,:,2]
 
-    maxc = np.maximum(np.maximum(r, g), b)  # gets maximum between R', G', and B'
-    minc = np.minimum(np.minimum(r, g), b)  # gets minimum between R', G', and B'
+    maxc = np.maximum(np.maximum(r, g), b)  # gets vector of the maximum of R', G', and B' for each pixel
+    minc = np.minimum(np.minimum(r, g), b)  # gets vector of the maximum of R', G', and B' for each pixel
     deltac = maxc - minc                    # gets difference between max(r,g,b) and min(r,g,b) [Δ]
 
     # getting V
     v = maxc
 
     # getting S
-    s = deltac / maxc
-    s[v==0] = 0
+    s = deltac / v   # proper equation (rather than deltac/maxc)
+    s[v==0] = 0      # catching case where S is undefined due to v being 0
 
     # getting h
     h = np.zeros((height, width), np.float32)
@@ -90,97 +37,84 @@ def rgb_to_hsv_1( rgb_image ):
     res = np.stack([h, s, v], axis=-1)
     return res
 
-# Takes an HSV image represented as a numpy array (hsv_image) 
-# And converts it into RGB format.
-def hsv_to_rgb_1( hsv_image ):
-    
-    original_shape = hsv_image.shape
-    hsv = hsv_image.reshape(-1, 3)
-    h = hsv[:, 0]
-    s = hsv[:, 1]
-    v = hsv[:, 2]
 
-    #####
+# Takes an RGB image represented as a numpy array (rgb_image)
+# And converts it into HSV format.
+def rgb_to_hsv_2( rgb_image ):
 
-    c = v * s
-    h_prime = h / 60
-    x = c * (1 - np.absolute(h_prime % 2 - 1) )
-    m = v - c
+    rgb_image = rgb_image / 255            # Normalizing to [0, 1]
+    rgb_image = rgb_image.reshape(-1, 3)   # Changing to a 3-column shape
+    r = rgb_image[:, 0]                    # Extracting the R' values to a vector
+    g = rgb_image[:, 1]                    # Extracting the G' values to a vector
+    b = rgb_image[:, 2]                    # Extracting the B' values to a vector
 
-    if 0 <= h_prime < 1:
-        r_prime = c
-        g_prime = x
-        b_prime = 0
-    elif 1 <= h_prime < 2:
-        r_prime = x
-        g_prime = c
-        b_prime = 0
-    elif 2 <= h_prime < 3:
-        r_prime = 0
-        g_prime = c
-        b_prime = x
-    elif 3 <= h_prime < 4:
-        r_prime = 0
-        g_prime = x
-        b_prime = c
-    elif 4 <= h_prime < 5:
-        r_prime = x
-        g_prime = 0
-        b_prime = c
-    elif 5 <= h_prime < 6:
-        r_prime = c
-        g_prime = 0
-        b_prime = x
+    max = np.maximum(r, g, b)  # gets vector of the maximum of R', G', and B' for each pixel
+    min = np.minimum(r, g, b)  # gets vector of the maximum of R', G', and B' for each pixel
 
-    r = r_prime + m
-    g = g_prime + m
-    b = b_prime + m
-
-##################
-
-
-
-def rgb_to_hsv(image):
-
-    image = image / 255
-    image = image.reshape(-1, 3)
-    r = image[:, 0]
-    g = image[:, 1]
-    b = image[:, 2]
-
-    max = np.maximum(r, g, b)
-    min = np.minimum(r, g, b)
-
+    # Getting V
     v = max
+
+    # Getting C
     c = v - min
+
+    # Getting S
     s = c / v
-    s[v==0] = 0
-    print("\nprinting v\n")
-    print(v)
-    print("\nprinting s\n")
-    print(s)
+    s[v==0] = 0  # catching case where S is undefined due to V being 0
 
-    '''
-    H' =
-    0, C=0
-    ( (G - B) / C ) mod 6, V=R
-    ( (B - R) / C ) + 2,   V=G
-    ( (R - G) / C ) + 4,   V=B
-    '''
-
-    h = np.zeros(image.shape[0])
-    # h = h.reshape(-1, 1)
+    h = np.zeros(rgb_image.shape[0])
     print("\nprinting h\n")
     print(h)
     h[c==0] = 0
-    h[v==r] = ( (g[v==r] - b[v==r]) / c ) % 6
-    h[v==g] = ( (b[v==g] - r[v==g]) / c ) + 2
-    h[v==b] = ( (r[v==b] - g[v==b]) / c ) + 4
+    h[v==r] = ( ( g[v==r] - b[v==r] ) / c ) % 6
+    h[v==g] = ( ( b[v==g] - r[v==g] ) / c ) + 2
+    h[v==b] = ( ( r[v==b] - g[v==b] ) / c ) + 4
     
     h = h * 60
 
-    res = np.stack([h, s, v], axis=-1)
-    print(res)
+    hsv_array = np.stack([h, s, v], axis=-1)
+    
+    return hsv_array
+
+
+# Takes an HSV image represented as a numpy array (hsv_image) 
+# And converts it into RGB format.
+def hsv_to_rgb( hsv_image ):
+    
+    # Extracting each channel
+    h = hsv_image[:, 0]
+    s = hsv_image[:, 1]
+    v = hsv_image[:, 2]
+
+    # Getting C (chroma) back
+    # Chroma (C) = V * S
+    c = v * s
+
+    # Getting H' back
+    h_prime = h / 60
+
+    # Getting X
+    # X = C * (1 - |H' % 2 - 1| )
+    x = c * ( 1 - np.absolute(h_prime % 2 - 1) )
+
+    # making array for RGB
+    rgb = np.zeros_like(hsv_image)
+
+    rgb[0 <= h_prime < 1] = np.hstack([c, x, 0])[0 <= h_prime < 1]
+    rgb[1 <= h_prime < 2] = np.hstack([x, c, 0])[1 <= h_prime < 2]
+    rgb[2 <= h_prime < 3] = np.hstack([0, c, x])[2 <= h_prime < 3]
+    rgb[3 <= h_prime < 4] = np.hstack([0, x, c])[3 <= h_prime < 4]
+    rgb[4 <= h_prime < 5] = np.hstack([x, 0, c])[4 <= h_prime < 5]
+    rgb[5 <= h_prime < 6] = np.hstack([c, 0, x])[5 <= h_prime < 6]
+
+    # Getting M for final modification
+    m = v - c
+
+    # Adding m back into each channel
+    rgb[:, 0] = rgb[:, 0] + m
+    rgb[:, 1] = rgb[:, 1] + m
+    rgb[:, 2] = rgb[:, 2] + m
+
+    return rgb
 
 
 # Accepts the following input from command line via sys.argv:
@@ -216,26 +150,16 @@ def main(argv, argc):
 
     # load image and convert to numpy array
     image = np.asarray( Image.open(filename) )
-    # print(image[305,223])
-    # print(image[259,127])
-    print(image)
 
-    image = image.reshape(-1, 3)
-    print("\n\n\n\n\n\n\n")
-    print(image)
+    # Capture image shape for reshaping later
+    image_shape = image.shape
 
-    # choice = input("\nWould you like to convert the image from RGB to HSV (1) or from HSV to RGB (2)? : ")
-
-    # if choice == "1":
-    image = rgb_to_hsv(image)
-    #     # print(image)
-    #     # image = hsv_to_rgb(image)
-    # elif choice == "2":
-    #     image = hsv_to_rgb(image)
+    # Conversion
+    image_mod1 = rgb_to_hsv_1(image)
+    image_mod2 = hsv_to_rgb(image_mod1)
 
     # show image (will save it later)
-    plt.imshow(image)
-    # plt.imshow('uss_enterprise.jpeg')
+    plt.imshow(image_mod2)
     plt.show()
 
 
--- assignment1/img_transforms.py
+++ assignment1/img_transforms.py
@@ -1,10 +1,24 @@
+from change_hsv import hsv_to_rgb, rgb_to_hsv_1
+
 from random import randint
+import random
 import sys
 import numpy as np
 from numpy.lib import stride_tricks
 import PIL
 from PIL import Image
 from matplotlib import pyplot as plt
+
+# WORKING
+#   random_crop
+#   resize_img
+
+# NOT WORKING
+#   extract_patch
+
+# UNTESTED
+#   color_jitter
+
 
 # Generates a random square crop of an image given the following inputs:
 # • An image as a numpy array.
@@ -20,17 +34,28 @@ def random_crop(img, size):
         print("improper size given")
         return
 
-    column = randint( (0 + size/2), (width - size/2)+1 )
-    row    = randint( (0 + size/2), (height - size/2)+1 )
+    # Generate random center coordinates for the crop
+    column = randint( (0 + size/2), ((width - size/2)+1)  )
+    row    = randint( (0 + size/2), ((height - size/2)+1) )
 
-    col_lo_bound = column - size/2
-    col_hi_bound = column + size/2
-    row_lo_bound = row    - size/2
-    row_hi_bound = row    + size/2
+    col_lo_bound = int( column - size/2 )
+    col_hi_bound = int( column + size/2 )
+    row_lo_bound = int( row    - size/2 )
+    row_hi_bound = int( row    + size/2 )
 
     random_cropped = img[row_lo_bound:row_hi_bound, col_lo_bound:col_hi_bound]
     
     return random_cropped
+
+
+# Testing above
+# Status: WORKING
+def test_random_crop(img, size):
+
+    random_cropped = random_crop(img, size)
+
+    plt.imshow(random_cropped)
+    plt.show()
 
 
 # Patch extraction
@@ -39,33 +64,149 @@ def random_crop(img, size):
 # a numpy array, as well as an integer n.
 # You may assume that the input image is square.
 def extract_patch(img, num_patches):
-    
+
     # non-overlapping patches of size (num_patches)
-    size = num_patches
-    H, W = img.shape
+    size = int( img.shape[0] / (num_patches*num_patches) )
+    H, W, D = img.shape
     shape = [H // size, W // size] + [size, size]
+    print("\n\nshape        = " + str(shape) )
 
     # (row, col, patch_row, patch_col)
-    strides = [size * s for s in img.strides] + list(img.strides)
+    print("img.strides  = " + str(img.strides))
+    strides = [size * s for s in img.strides[:2]] + list(img.strides[:2])
+    print("Stride Count = " + str(len(strides)) )
     # extract patches
+    print("Shape        = " + str(shape)   )
+    print("Strides      = " + str(strides) )
     patches = stride_tricks.as_strided(img, shape=shape, strides=strides)
+    print("Patch Count  = " + str(len(patches)) )
 
-    # not sure what the integer n is supposed to be?
-    n = size * size
+    for patch in patches:
+        print(patch.shape)    
     
     # return patches as array as well as an integer n
-    return patches, n
+    return patches, num_patches
+
+
+# Testing above
+# Status: IN TESTING
+#   Issue with dimensionality
+def test_extract_patch(img, num_patches):
+
+    extracted_patches, n = extract_patch(img, num_patches)
+
+    print("\n\n"+str(n)+"\n\n")
+
+    for patch in extracted_patches:
+        plt.imshow(patch)
+        plt.show()
 
 
 # Resizing function that resizes an image given an input image as a numpy array
 # and an integer representing the desired scale factor. 
 # The image should be resized using nearest neighbor interpolation.
-def resize_img(img, factor):
-    pass
+# Modified from answer at:
+# https://stackoverflow.com/questions/69728373/resize-1-channel-numpy-image-array-with-nearest-neighbour-interpolation
+def resize_img(img, scale_factor):
+    
+    shape = [img.shape[0], img.shape[1]]
+    
+    def per_axis(in_sz, out_sz):
+        ratio = 0.5 * in_sz / out_sz
+        return np.round(np.linspace(ratio - 0.5, in_sz - ratio - 0.5, num=out_sz)).astype(int)
+
+    return img[per_axis(img.shape[0], (shape[0]*scale_factor) )[:, None],
+               per_axis(img.shape[1], (shape[1]*scale_factor) )]
+
+# Testing above
+# Status: WORKING
+def test_resize_img(img, scale_factor):
+    resized_image = resize_img(img, scale_factor)
+
+    plt.imshow(resized_image)
+    plt.show()
 
 
 # Randomly perturbs the HSV values on an input image by an amount 
 #   no greater than the given input value. 
 # This should use your code from the first part of the assignment to modify the HSV channels.
 def color_jitter(img, hue, saturation, value):
-    pass
+
+    hsv_image = rgb_to_hsv_1(img)
+    
+    hsv_image[:, 0] = hsv_image[:, 0] + randint(        ( -1 * hue ),        hue        )  # Perturbing H channel
+    hsv_image[:, 1] = hsv_image[:, 1] + random.uniform( ( -1 * saturation ), saturation )  # Perturbing S channel
+    hsv_image[:, 2] = hsv_image[:, 2] + random.uniform( ( -1 * value ),      value      )  # Perturbing V channel
+
+    # Catch out-of-bounds for H channel
+    # hsv_image[:, 0] = 60[ hsv_image[:, 0] > 60 ]
+    # hsv_image[:, 0] = 0[ hsv_image[:, 0] < 0 ]
+    hsv_image[:, 0][ hsv_image[:, 0] > 60 ] = 60
+    hsv_image[:, 0][ hsv_image[:, 0] < 0  ] = 0
+
+    # Catch out-of-bounds for S channel
+    hsv_image[:, 1][ hsv_image[:, 1] > 1 ] = 1
+    hsv_image[:, 1][ hsv_image[:, 1] < 0 ] = 0
+
+    # Catch out-of-bounds for V channe
+    hsv_image[:, 2][ hsv_image[:, 2] > 1 ] = 1
+    hsv_image[:, 2][ hsv_image[:, 2] < 0 ] = 0
+
+    # Get newly-perturbed image
+    return hsv_to_rgb( hsv_image )
+
+    # Old attempts/scratch work
+    #
+    #
+
+    # hue_modified        = hue        + randint(        ( -1 * hue ),        hue        )
+    # saturation_modified = saturation + random.uniform( ( -1 * saturation ), saturation )
+    # value_modified      = value      + random.uniform( ( -1 * value ),      value      )
+
+    # if saturation_modified > 1:
+    #     saturation_modified = 1
+    # elif saturation_modified < 1:
+    #     saturation_modified = 0
+
+    # if value_modified > 1:
+    #     value_modified = 1
+    # elif value_modified < 1:
+    #     value_modified = 0
+
+    # if hue_modified > 60:
+    #     hue_modified = 60
+    # elif hue_modified < 0:
+    #     hue_modified = 0
+
+
+# Testing above
+# Status: UNTESTED
+def test_color_jitter(img, hue, saturation, value):
+
+    jittered_image = color_jitter(img, hue, saturation, value)
+
+    plt.imshow(jittered_image)
+    plt.show()
+
+
+# Main function
+def main(argv, argc):
+
+    filename             = argv[1]
+    option               = argv[2]
+
+    image = np.asarray( Image.open(filename) )
+
+    if option == "random_crop":
+        test_random_crop( image, int(argv[3]) )
+    elif option == "extract_patch":
+        test_extract_patch( image, int(argv[3]) )
+    elif option == "resize_img":
+        test_resize_img( image, int(argv[3]) )
+    elif option == "color_jitter":
+        test_color_jitter( image, int(argv[3]), int(argv[4]), int(argv[5]) )
+
+
+# Calling main()
+if __name__ == "__main__":
+    main( sys.argv, len(sys.argv) )
