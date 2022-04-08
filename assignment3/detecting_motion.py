f76e28506ae4c4885b01c95a0b7ad397449787ea
Michael Bonnet <maabonnet@gmail.com>
2022-04-08 12:12:14 -0500

    starting assignment 3

---
+++ assignment3/detecting_motion.py
@@ -0,0 +1,13 @@
+
+class MotionDetector(frame_1, frame_2, frame_3, alpha, tau, delta, s, N):
+    pass
+
+class KalmanFilter:
+
+    def predict():
+        pass
+
+    def update():
+        pass
+    
+    pass
